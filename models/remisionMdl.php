30a923eefdd56687199eb9a9430df5f83fb559e0
abimael93 <chris.abimael93@gmail.com>
2014-09-21 00:20:46 -0500

    Modificación del Modelo de Remision

--- models/remisionMdl.php
+++ models/remisionMdl.php
@@ -4,7 +4,6 @@ require_once 'models/baseMdl.php';
 	* Clase para el modelo de Remisas
 	*/
 class RemisionMdl extends BaseMdl{
-	private $idMovimientoAlmacen;
 	private $idCliente;
 	private $folio;
 	private $fechaRemision;
@@ -22,17 +21,25 @@ class RemisionMdl extends BaseMdl{
 	 *@param integer $idCliente
 	 *@param integer $folio
 	 *@param date $fechaRemision
-	 *@param decimal $total
+	 *@param array $idProductos
+	 *@param array $cantidades
+	 *@param array $precioUnitario
+	 *@param array $ivas
+	 *@param array $descuentos
 	 *Crea una nueva remision
 	 *@return true
 	 */
-	function create($idMovimientoAlmacen, $idCliente, $folio, $fechaRemision, $total){
-		$this->idMovimientoAlmacen = $idMovimientoAlmacen;
-		$this->idCliente = $idCliente;
-		$this->folio	= $folio;
+	function create($idCliente, $folio, $fechaRemision, $total,$idProductos,$cantidades,$precioUnitario,$ivas,$descuentos){
+		$this->idCliente 		= $idCliente;
+		$this->folio			= $folio;
 		$this->fechaRemision	= $fechaRemision;
-		$this->total	= $total;
-		
+		$total = 0;
+		for($i = 0;$i < count($idProductos);$i++){
+			if(!$this->createDetails(1,$idProductos[$i],$cantidades[$i],$precioUnitario[$i],$ivas[$i],$descuentos[$i]))
+				return false;
+			$total += $cantidades[$i]*$precioUnitario[$i];
+		}
+		$this->total = $total;
 		return true;
 	}
 	
@@ -46,13 +53,13 @@ class RemisionMdl extends BaseMdl{
 	 *Crea un nuevo detalle de una remision
 	 *@return true
 	 */
-	function createDetails($idRemision, $idProductoServicio, $cantidad, $idProductoServicio, $cantidad){
-		$this->idRemision = $idRemision;
+	function createDetails($idRemision,$idProductoServicio,$cantidad,$precioUnitario,$iva,$descuento){
+		$this->idRemision 		  = $idRemision;
 		$this->idProductoServicio = $idProductoServicio;
-		$this->cantidad	= $cantidad;
-		$this->precioUnitario	= $precioUnitario;
-		$this->iva	= $iva;
-		$this->descuento	= $descuento;
+		$this->cantidad			  = $cantidad;
+		$this->precioUnitario	  = $precioUnitario;
+		$this->iva				  = $iva;
+		$this->descuento		  = $descuento;
 		
 		return true;
 	}
