24b73b9d84c80ef7bc415f628630361ffa208d00
Alan Andrés Sánchez Castro <pony_alanandres@hotmail.com>
2014-09-21 13:11:36 -0500

    Se añadió una vista cuando el index llega sin parámetros

--- index.php
+++ index.php
@@ -1,60 +1,66 @@
 <?php
 	//Recibiendo
 	//Get query args
-	switch($_GET['ctrl']){
-		case 'ajusteEntrada':
-			//Crear el controlador y ejecutarlo
-			require('controllers/ajusteEntradaCtrl.php');
-			$ctrl = new AjusteEntradaCtrl();
-			break;
-		case 'ajusteSalida':
-			//Crear el controlador y ejecutarlo
-			require('controllers/ajusteSalidaCtrl.php');
-			$ctrl = new AjusteSalidaCtrl();
-			break;
-		case 'consulta':
-			//Crear el controlador y ejecutarlo
-			require('controllers/consultaCtrl.php');
-			$ctrl = new ConsultaCtrl();
-			break;
-		case 'empleado':
-			//Crear el controlador y ejecutarlo
-			require('controllers/empleadoCtrl.php');
-			$ctrl = new EmpleadoCtrl();
-			break;
-		case 'producto':
-			//Crear el controlador y ejecutarlo
-			require('controllers/productoCtrl.php');
-			$ctrl = new ProductoCtrl();
-			break;
-		case 'proveedor':
-			//Crear el controlador y ejecutarlo
-			require('controllers/proveedorCtrl.php');
-			$ctrl = new ProveedorCtrl();
-			break;
-		case 'recepcion':
-			//Crear el controlador y ejecutarlo
-			require('controllers/recepcionCtrl.php');
-			$ctrl = new RecepcionCtrl();
-			break;
-		case 'remision':
-			//Crear el controlador y ejecutarlo
-			require('controllers/remisionCtrl.php');
-			$ctrl = new RemisionCtrl();
-			break;
-		case 'servicio':
-			//Crear el controlador y ejecutarlo
-			require('controllers/servicioCtrl.php');
-			$ctrl = new ServicioCtrl();
-			break;
-		case 'historialMedico':
-			//Crear el controlador y ejecutarlo
-			require('controllers/historialMedicoCtrl.php');
-			$ctrl = new HistorialMedicoCtrl();
-			break;
-		default:
-			# code...
-			break;
+	if (isset($_GET['ctrl'])) {
+		switch($_GET['ctrl']){
+			case 'ajusteEntrada':
+				//Crear el controlador y ejecutarlo
+				require('controllers/ajusteEntradaCtrl.php');
+				$ctrl = new AjusteEntradaCtrl();
+				break;
+			case 'ajusteSalida':
+				//Crear el controlador y ejecutarlo
+				require('controllers/ajusteSalidaCtrl.php');
+				$ctrl = new AjusteSalidaCtrl();
+				break;
+			case 'consulta':
+				//Crear el controlador y ejecutarlo
+				require('controllers/consultaCtrl.php');
+				$ctrl = new ConsultaCtrl();
+				break;
+			case 'empleado':
+				//Crear el controlador y ejecutarlo
+				require('controllers/empleadoCtrl.php');
+				$ctrl = new EmpleadoCtrl();
+				break;
+			case 'producto':
+				//Crear el controlador y ejecutarlo
+				require('controllers/productoCtrl.php');
+				$ctrl = new ProductoCtrl();
+				break;
+			case 'proveedor':
+				//Crear el controlador y ejecutarlo
+				require('controllers/proveedorCtrl.php');
+				$ctrl = new ProveedorCtrl();
+				break;
+			case 'recepcion':
+				//Crear el controlador y ejecutarlo
+				require('controllers/recepcionCtrl.php');
+				$ctrl = new RecepcionCtrl();
+				break;
+			case 'remision':
+				//Crear el controlador y ejecutarlo
+				require('controllers/remisionCtrl.php');
+				$ctrl = new RemisionCtrl();
+				break;
+			case 'servicio':
+				//Crear el controlador y ejecutarlo
+				require('controllers/servicioCtrl.php');
+				$ctrl = new ServicioCtrl();
+				break;
+			case 'historialMedico':
+				//Crear el controlador y ejecutarlo
+				require('controllers/historialMedicoCtrl.php');
+				$ctrl = new HistorialMedicoCtrl();
+				break;
+			default:
+				require_once 'views/provisional.html';
+				break;
+		}
+	}else{
+		require_once 'views/provisional.html';
 	}
-	$ctrl->run();
+	if (isset($ctrl)) {
+		$ctrl->run();
+	}
 ?>
