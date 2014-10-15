d046d80c905ff15e9ad6340458599da30727815d
abimael93 <chris.abimael93@gmail.com>
2014-10-14 20:45:09 -0500

    Tabla EmpleadoSueldo
    Código SQL para crear la tabla EmpleadoSueldo

---
+++ Documentacion/TablaSueldo.sql
@@ -0,0 +1,7 @@
+CREATE TABLE EmpleadoSueldo(
+    IDSueldoEmpleado INT PRIMARY KEY AUTO_INCREMENT,
+    IDEmpleado INT NOT NULL,
+    FechaSueldo DATE NOT NULL,
+    Sueldo DECIMAL(10,2) NOT NULL DEFAULT 0,
+    FOREIGN KEY (IDEmpleado) REFERENCES Empleado(IDEmpleado)
+);
