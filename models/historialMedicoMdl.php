f2d051152375e9c9d8bfb3ab9232e91a45e559f6
abimael93 <chris.abimael93@gmail.com>
2014-11-17 02:41:20 -0600

    Modelo de Historial Médico ya crea
    Se siguen realizando modificaciones

--- models/historialMedicoMdl.php
+++ models/historialMedicoMdl.php
@@ -320,17 +320,16 @@ class HistorialMedicoMdl extends BaseMdl{
 
 	/**
 	*Actualiza la información de Agua al dia
-	*@param int $idAguaAlDia
 	*@param int $idHistorialMedico
 	*@param string $poca
 	*@param string $regularAg
 	*@param string $mucha
 	*@return true or false
 	**/
-	function updateAguaAlDia($idAguaAlDia,$idHistorialMedico,$poca,$regularAg,$mucha){
-		if($stmt = $this->driver->prepare('SELECT IDAguaAlDia FROM AguaAlDia WHERE IDAguaAlDia=?')){
+	function updateAguaAlDia($idHistorialMedico,$poca,$regularAg,$mucha){
+		if($stmt = $this->driver->prepare('SELECT IDHistorialMedico FROM AguaAlDia WHERE IDHistorialMedico=?')){
 		
-			if(!$stmt->bind_param('i',$idAguaAlDia))
+			if(!$stmt->bind_param('i',$idHistorialMedico))
 				return false;
 			
 			if(!$stmt->execute())
@@ -338,14 +337,14 @@ class HistorialMedicoMdl extends BaseMdl{
 				
 			$mySqliResult = $stmt->get_result();
 
-			if($mySqliResult->field_count > 0 && $mySqliResult->fetch_assoc()['IDAguaAlDia']!=''){
+			if($mySqliResult->field_count > 0 && $mySqliResult->fetch_assoc()['IDHistorialMedico']!=''){
 				$this->poca 	 = $this->driver->real_escape_string($poca);
 				$this->regularAg 	 = $this->driver->real_escape_string($regularAg);
 				$this->mucha 	 = $this->driver->real_escape_string($mucha);
 				
-				$stmt = $this->driver->prepare("UPDATE AguaAlDia SET IDHistorialMedico=?,Poca=?,Regular=?,Mucha=? WHERE IDAguaAlDia=?");
+				$stmt = $this->driver->prepare("UPDATE AguaAlDia SET Poca=?,Regular=?,Mucha=? WHERE IDHistorialMedico=?");
 
-				if(!$stmt->bind_param('isssi',$idHistorialMedico, $this->poca,$this->regularAg,$this->mucha,$idAguaAlDia)){
+				if(!$stmt->bind_param('sssi'$this->poca,$this->regularAg,$this->mucha,$idHistorialMedico)){
 					return false;
 				}
 				if (!$stmt->execute()) {
@@ -365,16 +364,16 @@ class HistorialMedicoMdl extends BaseMdl{
 	/**
 	* Consulta los registros de Consumo de Agua al Día registrados en el sistema
 	*@param int $offset
-	*@param int $idAguaAlDia
+	*@param int $idHistorialMedico
 	* @return array or false
 	**/
-	function listsAguaAlDia($offset = -1,$idAguaAlDia = -1, $constrain = '1 = 1'){
+	function listsAguaAlDia($offset = -1,$idHistorialMedico = -1, $constrain = '1 = 1'){
 		$rows = array();
 		if($offset>-1){
 			$stmt = $this->driver->prepare('SELECT * FROM AguaAlDia WHERE '.$constrain.' LIMIT ?,?');
 		}else{
-			if($idAguaAlDia>-1){
-				$stmt = $this->driver->prepare('SELECT * FROM AguaAlDia WHERE IDAguaAlDia=?');
+			if($idHistorialMedico>-1){
+				$stmt = $this->driver->prepare('SELECT * FROM AguaAlDia WHERE IDHistorialMedico=?');
 			}else{
 				$stmt = $this->driver->prepare('SELECT * FROM AguaAlDia WHERE '.$constrain);
 			}
@@ -385,8 +384,8 @@ class HistorialMedicoMdl extends BaseMdl{
 				$offset*=10;
 				if(!$stmt->bind_param('ii',$offset,$amountRows))
 					return false;
-			}else if($idAguaAlDia>-1){
-				if(!$stmt->bind_param('i',$idAguaAlDia))
+			}else if($idHistorialMedico>-1){
+				if(!$stmt->bind_param('i',$idHistorialMedico))
 					return false;
 			}
 			if(!$stmt->execute())
