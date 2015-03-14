9f088c7ee5e4302d494b1027657735e4e2029df1
Abimael93 <chris.abimael93@gmail.com>
2015-03-14 15:44:42 -0600

    Configuración del servidor de correos

--- controllers/baseCtrl.php
+++ controllers/baseCtrl.php
@@ -249,19 +249,32 @@
 		*	@return bool 
 		*/
 		public static function enviarCorreo($correo,$asunto,$body,$remplazos){
+			/*Lo primero es añadir al script la clase phpmailer desde la ubicación en que esté*/
 			require_once 'PHPMailer/PHPMailerAutoload.php';
 
+			//Crear una instancia de PHPMailer
 			$mail = new PHPMailer;
+			//Definir que vamos a usar SMTP
 			$mail->isSMTP();
+			//Esto es para activar el modo depuración. En entorno de pruebas lo mejor es 2, en producción siempre 0
+			// 0 = off (producción)
+			// 1 = client messages
+			// 2 = client and server messages
 			$mail->SMTPDebug = 0;
-			
-			$mail->Host = "mail.astrum.x10.mx";
-			$mail->Port = 25;
+			//Ahora definimos gmail como servidor que aloja nuestro SMTP
+			$mail->Host = "smtp.gmail.com";
+			//El puerto será el 587 ya que usamos encriptación TLS
+			$mail->Port = 587;
+			//Definmos la seguridad como TLS
+			$mail->SMTPSecure = 'tls';
+			//Tenemos que usar gmail autenticados, así que esto a TRUE
 			$mail->SMTPAuth = true;
-			$mail->Username = "contacto@astrum.x10.mx";
-			$mail->Password = "astrum1234";
-			
-			$mail->setFrom('contacto@astrum.x10.mx', 'SpaDamaris');	
+			//Definimos la cuenta que vamos a usar. Dirección completa de la misma
+			$mail->Username = "spadamaris.test@gmail.com";
+			//Introducimos nuestra contraseña de gmail
+			$mail->Password = "spadamaris.test11";
+			//Definimos el remitente (dirección y, opcionalmente, nombre)
+			$mail->setFrom('spadamaris.test@gmail.com', 'SpaDamaris');	
 			$mail->addAddress($correo);
 
 			$mail->Subject = 'SpaDamaris: '.$asunto;
